a7235f08e8e1efc3c23572cf78cfce0663ab53ea
mudbungie <mudbungie@gmail.com>
2020-11-07 12:27:26 -0800

    stashing java nonsense

---
+++ learning/java/MyClass.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String[] args) {
+        // c-style comments
+        /* 
+            also, c++ style comments
+        */
+        String message = "yo";
+        System.out.println(message);
+        final float num = 1.5f;
+        //int num = 14;
+        System.out.println(message + num);
+        System.out.println(message.length());
+    }
+}
