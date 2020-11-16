b458a969c5b87b2020b08a6747a64f6e72cf0b2d
mudbungie <mudbungie@gmail.com>
2020-11-16 13:44:10 -0800

    more futzing with c

--- c/whatever.c
+++ c/whatever.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void main(){
-    printf("whatever\n");
+    bool desired_truthiness = true;
+
+    int thing = 4;
+    bool truthy = false;
+    if ( ! desired_truthiness ){
+        printf("yes");
+        int truthy = 1;
+    }
+    else {
+        printf("no");
+        int truthy = 2;
+    }
+
+    //printf("%d", thing);
+    //printf("%d", 4 == thing);
+    printf("%d", truthy);
+    printf("\n");
 }
