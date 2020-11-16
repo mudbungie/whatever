afe1674e3a8afd69643c96c37c0e264295222592
mudbungie <mudbungie@gmail.com>
2020-11-16 15:21:47 -0800

    and more

--- c/whatever.c
+++ c/whatever.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
 void main(){
-    for ( int i = 0; i < 5; i++){
+    int i = 0;
+    do {
         printf("%d", i);
-    }
+        i++;
+    } while (i < 5);
+
     printf("\n");
 }
