8f70031299ba0463d950fc8ce8ba38f574471498
mudbungie <mudbungie@gmail.com>
2022-04-28 18:45:25 -0700

    the stupidest thing

--- do_stupid_things_fast.py
+++ do_stupid_things_fast.py
@@ -1,14 +1,16 @@
 from random import choices, randint
 from subprocess import run
 from string import printable
+from keyword import kwlist
 from sys import argv
 
-def build_program(length: int, max_word_length: int=10) -> str:
+
+def build_program(lines: int, max_word_length: int = 10) -> str:
     program = ""
-    
+
     while len(program) < length:
-        new_word_length = k=randint(1, max_word_length)
-        new_word = ''.join(choices(printable, k=new_word_length))
+        new_word_length = k = randint(1, max_word_length)
+        new_word = " ".join(choices([*printable, *kwlist], k=new_word_length))
 
         if is_valid_python(program + new_word):
             program += new_word
@@ -17,8 +19,9 @@ def build_program(length: int, max_word_length: int=10) -> str:
 
 def is_valid_python(program: str) -> bool:
     print(program)
-    attempt = run(["python3", "-c", program, '/dev/null'], capture_output=True)
+    attempt = run(["python3", "-c", program, "/dev/null"], capture_output=True)
     return attempt.returncode == 0
+
 
 if __name__ == "__main__":
     print(build_program(int(argv[1])))
